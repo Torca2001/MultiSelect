3aca9d546730b578d5b8238ca56df1824faf2741
Torca <Torca2001@gmail.com>
2022-12-16 11:57:45 +1100

    Drag selection Added ability to drag select multiple people

--- src/index.js
+++ src/index.js
@@ -18,14 +18,19 @@ module.exports = (Plugin, Library) => {
     const voiceStates = WebpackModules.getByProps("getVoiceStatesForChannel");
     const voiceUserComponent = WebpackModules.findByDisplayName('VoiceUser');
     const voiceUserSelector = BdApi.findModuleByProps("voiceUser").voiceUser;
-    const voiceUsersRender = ZLibrary.WebpackModules.getByPrototypes("renderPrioritySpeaker");
+    const voiceUsersRender = WebpackModules.getByPrototypes("renderPrioritySpeaker");
     const channelItemComponent = WebpackModules.getModule(m => Object(m.default).displayName === "ChannelItem");
-    //renderVoiceUsers
+    const guildChannelStore = WebpackModules.getByProps("getVocalChannelIds");
+    const selectedGuildStore = WebpackModules.getModuleByName("SelectedGuildStore")
+    const sortedVoiceStatesStore = WebpackModules.getByProps("countVoiceStatesForChannel")
 
     return class MultiSelect extends Plugin {
         cancelled = false;
         guild_id = "";
         selectedUsers = new Set();
+        mouseHeld = false;
+        modifierAddMode = true;
+        mouseStart = {x:0, y:0};
 
         constructor() {
             super();
@@ -45,14 +50,27 @@ module.exports = (Plugin, Library) => {
             this.PatchAll();
         }
 
+        addDocumentListenerEvent(event, callback) {
+            let unpatch = () => {
+                document.removeEventListener(event, callback);
+            };
+            document.addEventListener(event, callback);
+            return unpatch;
+        }
+
         async PatchAll() {
             this.contextMenuPatches.push(BdApi.ContextMenu.patch("channel-context", this.channelMenuPatch.bind(this)));
             
             Patcher.after(voiceUsersRender.prototype, "render", this.voiceUserRenderPatch.bind(this));
+            this.contextMenuPatches.push(this.addDocumentListenerEvent("mousedown", this.onMouseDown.bind(this)));
+            this.contextMenuPatches.push(this.addDocumentListenerEvent("mousemove", this.onMouseMove.bind(this)));
+            this.contextMenuPatches.push(this.addDocumentListenerEvent("mouseup", this.onMouseUp.bind(this)));
 
             return;
             Patcher.after(voiceUserComponent.prototype, "render", this.voiceUserRenderPatch.bind(this));
             Patcher.after(channelItemComponent, "default", this.ChannelItemPatch.bind(this));
+
+            // BdApi.Webpack.getModule(BdApi.Webpack.Filters.byStrings("FocusRing was given"), {searchExports: true, first: false})
 
             //Force update
             document.querySelectorAll(DiscordSelectors.ChannelList.containerDefault).forEach(e => {
@@ -72,6 +90,70 @@ module.exports = (Plugin, Library) => {
                     //Do nothing
                 }
             }
+        }
+
+        onMouseDown(e) {
+            if ((e.button == 0 || e.button == 2) && (e.shiftKey || e.ctrlKey)){
+                this.mouseHeld = true;
+                this.modifierAddMode = e.button == 0;
+                this.mouseStart = {x: e.x, y: e.y};
+
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                e.preventDefault();
+            }
+        }
+
+        onMouseUp(e) {
+            if (this.mouseHeld && (e.button == 0 || e.button == 2)){
+                this.mouseHeld = false;
+
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                e.preventDefault();
+
+                let voiceElements = document.querySelectorAll(".dragSelectedVoiceUser");
+
+                for (const element of voiceElements) {
+                    element.classList.toggle("dragSelectedVoiceUser", false);
+
+                    let user = ReactTools.getOwnerInstance(element.parentElement).props.user;
+                    if (user && user.id) {
+                        element.classList.toggle("selectedVoiceUser", this.modifierAddMode);
+                        if (this.modifierAddMode) {
+                            this.selectedUsers.add(user.id);
+                        } else {
+                            this.selectedUsers.delete(user.id);
+                        }
+                    }
+                }
+            }
+        }
+
+        onMouseMove(e) {
+            if (this.mouseHeld && (e.shiftKey || e.ctrlKey)) {
+                //dragSelectedVoiceUser
+                let top = Math.min(e.y, this.mouseStart.y);
+                let left = Math.min(e.x, this.mouseStart.x);
+                let right = Math.max(e.x, this.mouseStart.x);
+                let bottom = Math.max(e.y, this.mouseStart.y);
+
+                let voiceElements = document.querySelectorAll("." + voiceUserSelector);
+
+                for (const element of voiceElements) {
+                    let rect = element.getBoundingClientRect();
+                    // check if this rectangle collides with the selection area
+                    let collided = !(rect.left > right || rect.right < left || rect.top > bottom || rect.bottom < top)
+                    element.classList.toggle("dragSelectedVoiceUser", collided);
+                }
+
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                e.preventDefault();
+            } else {
+                this.mouseHeld = false;
+            }
+            //console.log(e);
         }
 
         channelMenuPatch(retVal, props) {
@@ -160,10 +242,12 @@ module.exports = (Plugin, Library) => {
         }
 
         voiceUserRenderPatch(org, args, resp) {
+            // BdApi.Webpack.getModule(BdApi.Webpack.Filters.byProps("getVoiceStates"))
+            // BdApi.Webpack.getModule(BdApi.Webpack.Filters.byProps("getVocalChannelIds"), {first: false})[0].getVocalChannelIds("268579081950330880")
+
             let oldfunc = resp.props.onClick;
             resp.props.onClick = (e) => {
                 if (e.ctrlKey || e.shiftKey) {
-
                     if (org.props.guildId != this.guild_id) {
                         this.guild_id = org.props.guildId;
                         this.selectedUsers.clear();
@@ -332,6 +416,11 @@ module.exports = (Plugin, Library) => {
         }
 
         css = `
+          .dragSelectedVoiceUser>div {
+            background-color: #0099ff87 !important;
+            border-radius: 5px;
+          }
+
           .selectedVoiceUser>div {
             background-color: #005fff87;
             border-radius: 5px;
